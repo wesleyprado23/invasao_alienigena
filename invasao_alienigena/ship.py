ef20f5620476667a1d5817e723a6324ff7bde28c
wesleyprado23 <wesley.prado93@pm.me>
2025-04-09 20:05:30 -0300

    Limite de tela

--- invasao_alienigena/ship.py
+++ invasao_alienigena/ship.py
@@ -43,9 +43,9 @@ class Ship:
 
     def update(self):
         """Atualiza a posição da espaçonave com base nas flags de movimento."""
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
-        if self.moving_left:
+        if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
 
         self.rect.x = self.x
